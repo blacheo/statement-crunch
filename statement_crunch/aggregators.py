ed973aac2d390d940a4a38cba19b7a0debecb7e1
brandon <blaicheo@uwaterloo.ca>
2023-12-17 21:54:50 -0500

    refactor: reformated test

--- statement_crunch/aggregators.py
+++ statement_crunch/aggregators.py
@@ -3,9 +3,7 @@ from typing import Dict, List
 from statement_crunch.credit_statement import Entry
 
 
-
-
-def monthly_sum_aggregator(entries: List[Entry]) -> dict[(int, int): int]:
+def monthly_sum_aggregator(entries: List[Entry]) -> dict[(int, int):int]:
     month_and_sum = dict()
     for entry in entries:
         if month_and_sum.get((entry.month, entry.year)) is not None:
