fb9bfd2d1012f969e523766c3e1cdfec3679af5d
brandon <blaicheo@uwaterloo.ca>
2023-12-17 21:55:30 -0500

    refactor: reformated file

--- statement_crunch/credit_statement_view.py
+++ statement_crunch/credit_statement_view.py
@@ -4,8 +4,6 @@ import numpy as np
 from statement_crunch.credit_statement import bmo_statement_pull_payments
 
 
-
-
 st.title("Credit Statement Cruncher")
 
 pdfs = st.file_uploader("Upload Credit Card Statements Here", True)
@@ -15,6 +13,6 @@ if pdfs is not None:
     entries = []
     for pdf in pdfs:
         entries.extend(bmo_statement_pull_payments(pdf))
-    
+
     monthly_sums = []
     np.bar()
